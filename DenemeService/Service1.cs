1dbb8ee71c947461586fa7a1db496d460432203b
osmangrel <osmangrel26@gmail.com>
2023-01-17 11:51:20 +0300

    Comments removed

--- DenemeService/Service1.cs
+++ DenemeService/Service1.cs
@@ -26,14 +26,9 @@ namespace DenemeService
         {
             try
             {
-                // Grab the Scheduler instance from the Factory 
                 IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
-
-                // and start it off
                 scheduler.Start();
-
                 WriteLogFile("Scheduler is started");
-                // define the job and tie it to our HelloJob class
                 IJobDetail job = JobBuilder.Create<MailSendMarketPrice>()
                     .WithIdentity("MAIL_SEND_MARKETPRICE_JOB", "group1")
                     .Build();
@@ -41,8 +36,6 @@ namespace DenemeService
                 IJobDetail job2 = JobBuilder.Create<DifferentJob>()
                    .WithIdentity("DIFFERENT_JOB", "group1")
                    .Build();
-
-                // Trigger the job to run now, and then repeat every 10 seconds
                 //0 30 15 * * ? her gün 15:30 da çalış
                 //https://www.freeformatter.com/cron-expression-generator-quartz.html
 
@@ -63,11 +56,8 @@ namespace DenemeService
                  .ForJob(job2)
                  .Build();
 
-                // Tell Quartz to schedule the job using our trigger
-
                 scheduler.ScheduleJob(job, trigger);
                 scheduler.ScheduleJob(job2, trigger2);
-
 
                 var nextFireTime = trigger.GetNextFireTimeUtc();
                 if (nextFireTime != null)
@@ -75,8 +65,6 @@ namespace DenemeService
                 var nextFireTime2 = trigger2.GetNextFireTimeUtc();
                 if (nextFireTime2 != null)
                     WriteLogFile("Second Job Next Fire Time: " + nextFireTime2.Value.LocalDateTime.ToString());
-
-
             }
             catch (SchedulerException se)
             {
@@ -93,7 +81,6 @@ namespace DenemeService
             }
             private static async Task runJob()
             {
-                //httpgetset url
                 WriteLogFile("MailSendMarketPrice working. I am First Job.");
             }
         }
@@ -105,7 +92,6 @@ namespace DenemeService
             }
             private static async Task runJob()
             {
-                //httpgetset url
                 WriteLogFile("DifferentJob working. I am Second Job.");
             }
         }
@@ -117,7 +103,7 @@ namespace DenemeService
         public static void WriteLogFile(string message)
         {
             StreamWriter sw = null;
-            sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\OsmanCronLogFile.txt", true);
+            sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\CronLogFile.txt", true);
             sw.WriteLine($"{DateTime.Now.ToString()} : {message}");
             sw.Flush();
             sw.Close();
